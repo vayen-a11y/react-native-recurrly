cce681edf4346ddc6ea3f9087deff2c26ba7620d
vayen-a11y <vayen@intern-mail.metabox.technology>
2026-09-10 12:24:51 +0400

    implement code-rabbit suggested fixes

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -57,7 +57,7 @@ export default function App() {
                                 />
                             </View>
 
-                            <ListHeading title="All Subscription" />
+                            <ListHeading title="All Subscriptions" />
 
                         </>
                     ) }
